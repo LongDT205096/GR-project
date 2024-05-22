66e0617f3c3b42f2652037d1cc09918b240870ed
LongDT205096 <dinhthanhlong0@gmail.com>
2024-05-23 04:19:40 +0700

    add api for rate and review

--- backend/backend/Apps/Rate/models.py
+++ backend/backend/Apps/Rate/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
-from ..Account.models import Account
-from ..Movie.models import Movie
 
 
 # Create your models here.
 class Rate(models.Model):
-    account = models.ForeignKey(Account, on_delete=models.CASCADE)
-    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    account = models.ForeignKey("Account.Account", on_delete=models.CASCADE)
+    movie = models.ForeignKey("Movie.Movie", on_delete=models.CASCADE)
     rate = models.IntegerField(default=0, validators=[MaxValueValidator(5), MinValueValidator(1)])
--- backend/backend/Apps/Rate/views.py
+++ backend/backend/Apps/Rate/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from ..Movie.models import Movie
 from .models import Rate
@@ -8,23 +8,38 @@ from .serializer import RateSerializer
 
 
 # Create your views here.
-class RateView(APIView):
-    permission_classes = [AllowAny]
+class RatePersonal(APIView):
+    permission_classes = [IsAuthenticated]
 
     def post(self, request):
         data = request.data
         movie = Movie.objects.get(pk=data['movie'])
         serializer = RateSerializer(data=data)
         if serializer.is_valid():
+            print(movie.average_rating())
+            movie.ave_rate = movie.average_rating()
             serializer.save()
+            movie.save()
             return Response({'message': 'Rate created'}, status=201)
         return Response(serializer.errors, status=400)
 
-    def put(self, request, pk):
-        data = request.data
+
+class RateEdit(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, pk):
         rate = Rate.objects.get(pk=pk)
-        serializer = RateSerializer(rate, data=data)
+        serializer = RateSerializer(rate)
+        return Response(serializer.data, status=200)
+
+    def put(self, request, pk):
+        rate = Rate.objects.get(pk=pk)
+        print(rate)
+        movie = Movie.objects.get(pk=request.data['movie'])
+        serializer = RateSerializer(rate, data=request.data)
         if serializer.is_valid():
             serializer.save()
+            movie.ave_rate = movie.average_rating()
+            movie.save()
             return Response({'message': 'Rate updated'}, status=200)
         return Response(serializer.errors, status=400)
